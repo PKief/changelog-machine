a89e5584bb097cd8eaf02c64a3df9fb906dd8219
Philipp Kief <philipp.kief@gmx.de>
2022-02-06 15:42:47 +0100

    Make config optional

--- src/cli/commands/printMarkdown.ts
+++ src/cli/commands/printMarkdown.ts
@@ -4,16 +4,16 @@ import { createMarkdown } from '../../utils/markdown';
 import { writeFileAsync } from '../../utils/promisify';
 import { defaultConfig } from '../config/default';
 
-const printMarkdown = async (config: Config) => {
-  const tagGroups = await groupCommitsByTags(config.blacklistPattern);
+const printMarkdown = async (config?: Config) => {
+  const tagGroups = await groupCommitsByTags(config?.blacklistPattern);
 
-  const title = `### ${config.title ?? defaultConfig.title} \n\n ${
-    config.description ?? defaultConfig.description
+  const title = `### ${config?.title ?? defaultConfig.title} \n\n ${
+    config?.description ?? defaultConfig.description
   }\n\n`;
-  const output = createMarkdown(title, tagGroups, config.repoName);
+  const output = createMarkdown(title, tagGroups, config?.repoName);
 
   await writeFileAsync(
-    config.outputFilename ?? defaultConfig.outputFilename,
+    config?.outputFilename ?? defaultConfig.outputFilename,
     output
   );
 };
